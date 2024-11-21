19327afb8925464e94bdf5496e0398effb0d1be2
Kristián Kunc <kristian.kunc@gmail.com>
2024-11-21 10:48:46 +0100

    add fib

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-import sys
-
-sys.exit()
+def fib(n):
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
